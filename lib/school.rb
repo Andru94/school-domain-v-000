975c82c0404cb22ec936f843b86af37cc8e4c59d
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-24 06:02:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+
+  attr_accessor :name, :grade
+
+  def initialize(name)
+    @name = name
+  end
+
+  def roster
+    hash = []
+  end
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    hash[@grade] = []
+    hash[@grade] << @name
+  end
+
+end
